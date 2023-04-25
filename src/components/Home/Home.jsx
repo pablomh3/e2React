f8df78dc1ad9c378e503356f118ff05ddc9b81bc
pablomh3 <pablonudenberg@gmail.com>
2023-04-24 23:40:14 -0300

    final

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { HomeContainer } from './HomeStyles'
+
 
 const Home = () => {
   return (
     <>
-  
-    <h1>Home</h1>
+      <HomeContainer>
+        <h1>¡Bienvenido!</h1>
+        <h3> El To Do List del Navbar aparecerá en rojo cuando hayan tareas almacenadas</h3>
+        <h4>Quizás necesites recargar la página para que cambie de color</h4>
+      </HomeContainer>
     </>
     
   )
---
+++ src/components/Home/HomeStyles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+display: flex;
+margin: 40px auto;
+align-items: center;
+justify-content: center;
+padding: 20px;
+width: 300px;
+flex-direction: column;
+h1{
+    text-align: center;
+    color: white;
+    font-size: 36px;
+    border-radius: 20px;
+    border: 1px solid white;
+    padding: 10px;
+}
+
+h3{
+    text-align: center;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+h4{
+    text-align: center;
+    color: red;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+`
